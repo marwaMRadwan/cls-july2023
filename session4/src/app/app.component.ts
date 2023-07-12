f999903c6ed6f1e3c887262ef8c9e2fb0c65fce6
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-07-12 12:55:03 +0300

    s4

---
+++ session4/src/app/app.component.ts
@@ -0,0 +1,124 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'session4';
+  // x = 5
+  // y = 3
+  // main = "main-color"
+  // user= {
+  //   name:"marwa",
+  //   age: 38
+  // }
+  // imgSrc= "http://techsexperts.com/storage/app/public/settings/DK5JPARF15PV0eXjsQ2yqZz9RSK57AfqMA8WoJRj.png"
+
+  // isDisabled = false
+
+  // hello(){
+  //   return "Hello from HTML file"
+  // }  
+  // a(){
+  //   alert("hello")
+  // }
+
+  // counter = 1
+  // increase(){
+  //   this.counter++
+  // }
+  // decrease(){
+  //   this.counter--
+  // }
+  // myDisplay = "block"
+  // msg="hide"
+  // showHide(){
+  //   if(this.myDisplay=="block") {
+  //     this.myDisplay="none"
+  //     this.msg="show"
+  //   }
+  //   else {
+  //     this.myDisplay="block"
+  //     this.msg="hide"
+  //   }
+  // }
+
+  // val = ""
+  // handleData(myVal:any){
+  //   this.val = myVal.value
+  // }
+  monthNum=1
+  data = [
+    {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+    },
+    {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796"
+    },
+    {
+    albumId: 1,
+    id: 3,
+    title: "officia porro iure quia iusto qui ipsa ut modi",
+    url: "https://via.placeholder.com/600/24f355",
+    thumbnailUrl: "https://via.placeholder.com/150/24f355"
+    },
+    {
+    albumId: 1,
+    id: 4,
+    title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    url: "https://via.placeholder.com/600/d32776",
+    thumbnailUrl: "https://via.placeholder.com/150/d32776"
+    },
+    {
+    albumId: 1,
+    id: 5,
+    title: "natus nisi omnis corporis facere molestiae rerum in",
+    url: "https://via.placeholder.com/600/f66b97",
+    thumbnailUrl: "https://via.placeholder.com/150/f66b97"
+    },
+    {
+    albumId: 1,
+    id: 6,
+    title: "accusamus ea aliquid et amet sequi nemo",
+    url: "https://via.placeholder.com/600/56a8c2",
+    thumbnailUrl: "https://via.placeholder.com/150/56a8c2"
+    },
+    {
+    albumId: 1,
+    id: 7,
+    title: "officia delectus consequatur vero aut veniam explicabo molestias",
+    url: "https://via.placeholder.com/600/b0f7cc",
+    thumbnailUrl: "https://via.placeholder.com/150/b0f7cc"
+    },
+    {
+    albumId: 1,
+    id: 8,
+    title: "aut porro officiis laborum odit ea laudantium corporis",
+    url: "https://via.placeholder.com/600/54176f",
+    thumbnailUrl: "https://via.placeholder.com/150/54176f"
+    },
+    {
+    albumId: 1,
+    id: 9,
+    title: "qui eius qui autem sed",
+    url: "https://via.placeholder.com/600/51aa97",
+    thumbnailUrl: "https://via.placeholder.com/150/51aa97"
+    },
+    {
+    albumId: 1,
+    id: 10,
+    title: "beatae et provident et ut vel",
+    url: "https://via.placeholder.com/600/810b14",
+    thumbnailUrl: "https://via.placeholder.com/150/810b14"
+    }
+    ]
+}
--- session4/src/app/app.module.ts
+++ session4/src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
