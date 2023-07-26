a07f5befda6f01b7ec985c4bedb45383bc7954e5
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-07-26 12:52:25 +0300

    s8

--- s8/src/app/components/pages/user/login/login.component.ts
+++ s8/src/app/components/pages/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/providers/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(private _data:UserService, private toastr: ToastrService, private _route:Router){}
+  loginForm= new FormGroup({
+    phone: new FormControl(),
+    fcm_token: new FormControl()
+  })
+  logMe(){
+    this._data.login(this.loginForm.value).subscribe(
+      res=>{
+        console.log(res)
+        if( res.error =="Unauthorized"){
+          this.toastr.error('Invalid Credintials', 'Marti Zoom Said');
+        }
+        else{
+          this._route.navigate(["/"])
+        }
+      } 
+      
+    )
+  }
 }
